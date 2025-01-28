1bdffcb3558d49982d00e2390cb0781a4a59f721
aikaterna <20862007+aikaterna@users.noreply.github.com>
2025-01-28 13:37:03 -0800

    Update __init__.py

--- sysinfo/__init__.py
+++ sysinfo/__init__.py
@@ -8,6 +8,6 @@ __red_end_user_data_statement__ = (
 def setup(bot):
     if psutilAvailable:
         n = SysInfo(bot)
-        bot.add_cog(n)
+        await bot.add_cog(n)
     else:
         raise RuntimeError(f"You need to run '{sys.executable} -m pip install psutil'")
